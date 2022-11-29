64bd34b8e44dbb4c8a3881f59400f43034a1245f
Julius Adrian <juliusa368@gmail.com>
2022-11-29 23:42:25 +0700

    First Commit

---
+++ app/src/main/java/com/example/aplikasifirebase/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.aplikasifirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pindah(View view) {
+        switch(view.getId()){
+            case R.id.add_mhs:
+                Intent moveAdd = new Intent(MainActivity.this, TambahMhsActivity.class);
+                startActivity(moveAdd);
+                break;
+            case R.id.list_mhs:
+                Intent moveList = new Intent(MainActivity.this, ListMhsActivity.class);
+                startActivity(moveList);
+                break;
+        }
+    }
+}
